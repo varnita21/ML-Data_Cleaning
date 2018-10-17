483f98f0016e7bcd852d90d741220c996b69ae47
Varnita Jain <varnita.jain.vj@gmail.com>
2018-10-17 11:28:34 -0700

    Add files via upload

---
+++ DataCleaning.py
@@ -0,0 +1,87 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+df= pd.read_csv("breast-cancer")
+
+
+# In[5]:
+
+
+df= pd.read_csv("breast-cancer.csv",encoding="utf8")
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+df.columns = df.columns.str.upper()
+
+
+# In[8]:
+
+
+df.columns
+
+
+# In[9]:
+
+
+df.head()
+
+
+# In[10]:
+
+
+df.isnull()
+
+
+# In[11]:
+
+
+df.isnull().any()
+
+
+# In[12]:
+
+
+df.isnull().any().any()
+
+
+# In[13]:
+
+
+df.isnull().sum()
+
+
+# In[14]:
+
+
+df.isnull().sum().sum()
+
+
+# In[17]:
+
+
+df_with_0 = df.fillna(0)
+
+
+# In[18]:
+
+
+df_with_0.head()
+
